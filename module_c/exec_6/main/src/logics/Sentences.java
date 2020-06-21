f3ba8ae3e0562417d21cd3725472605c3bb02bf2
Archi93 <ablaszczyk3@gmail.com>
2020-06-21 07:18:53 +0200

    exec6 out update and Sentences.java

--- module_c/exec_6/main/src/logics/Sentences.java
+++ module_c/exec_6/main/src/logics/Sentences.java
@@ -6,10 +6,10 @@ public class Sentences {
 
         // W pierwszej kolejności zapoznaj się ze prawdami o Kleofiksie
 
-        boolean sleptWell = false;              // Jak spał dzisiaj Kleofiks?
-        boolean ateBreakfast = true;            // Kleofiks zjadł śniadanie?
-        boolean drankMorningCoffee = true;      // Kleofiks napił się porannej kawy?
-        boolean readyToWork = false;            // Czy Kleofiks jest gotowy do pracy?
+        boolean sleptWell = true;              // Jak spał dzisiaj Kleofiks?
+        boolean ateBreakfast = false;            // Kleofiks zjadł śniadanie?
+        boolean drankMorningCoffee = false;      // Kleofiks napił się porannej kawy?
+        boolean readyToWork = true;            // Czy Kleofiks jest gotowy do pracy?
 
         // Podstawowe informacje o Kleofiksie
         System.out.println("Kleofiks spał dobrze? " + sleptWell);
@@ -26,28 +26,32 @@ public class Sentences {
 
         // 1.
         System.out.print("Kleofiks zjadł śniadanie lub wypił poranną kawę? ");
-        System.out.println();
+        System.out.println(ateBreakfast | drankMorningCoffee);
 
         // 2.
         System.out.print("Kleofiks spał dobrze lub wypił poranną kawę? ");
-        System.out.println();
+        System.out.println(sleptWell | drankMorningCoffee);
 
         // 3.
         System.out.print("Kleofiks nie spał dobrze i wypił poranną kawę? ");
-        System.out.println();
+        System.out.println(!sleptWell & drankMorningCoffee);
 
         // 4.
         System.out.print("Kleofiks nie jest gotowy do pracy i nie wypił porannej kawy? ");
-        System.out.println();
+        System.out.println(!readyToWork & !drankMorningCoffee);
 
         // 5.
         System.out.print("Kleofiks albo wypił poranną kawę albo zjadł śniadanie i jest gotowy do pracy? ");
-        System.out.println();
+        System.out.println(drankMorningCoffee ^ (ateBreakfast & readyToWork));
 
         // 6.
         System.out.print("Kleofiks spał dobrze i jest gotowy do pracy lub wypił poranną kawę i jest gotowy do pracy? ");
-        System.out.println();
+        System.out.println((sleptWell & readyToWork) | (drankMorningCoffee & readyToWork));
 
         // Analogicznie jak w powyższych przykładach dopisz dwa własne zdania logiczne dotyczące Kleofiksa
+        
+        int a = 1;
+boolean b = false && 2 > (a = 3);
+System.out.println(a);
     }
 }
